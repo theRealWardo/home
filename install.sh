1a2eda67ae75991b4c9e0a80ab004c6bf16dfda8
Matt Ward <contact@mwdesigns.com>
2018-02-04 22:19:53 +0000

    update install.sh to do more

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+mv .vim ~/.vim
+mv .vimrc ~/.vimrc
+mv .bashrc ~/.bashrc
+mv .bash_colors ~/.bash_colors
+mv .screenrc ~/.screenrc
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +VundleInstall +qa
 sudo apt-get install -y build-essential cmake python-dev python3-dev
