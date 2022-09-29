3fb02aeb393808a9429c530bfa51f75033afb9bb
Matt Ward <contact@mwdesigns.com>
2022-09-29 11:49:05 -0700

    mac support

--- install.sh
+++ install.sh
@@ -11,8 +11,15 @@ mv .bashrc ~/.bashrc
 mv .bash_colors ~/.bash_colors
 mv .screenrc ~/.screenrc
 
+if [[ `uname` = "Darwin" ]]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  brew doctor
+  brew install cmake
+else
+  sudo apt-get install -y build-essential cmake python-dev python3-dev
+fi
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +VundleInstall +qa
-sudo apt-get install -y build-essential cmake python-dev python3-dev
 cd ~/.vim/bundle/YouCompleteMe
 ./install.py --clang-completer --go-completer
